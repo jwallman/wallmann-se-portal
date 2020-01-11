40fe01a2a1a5515528c83a4465a91aa3a8ec1023
Jonas Wallmann <jonas.wallmann@gmail.com>
2020-01-11 18:05:35 +0100

    initial import

---
+++ src/main/java/com/wallmannse/portal/WallmannSePortalApplication.java
@@ -0,0 +1,13 @@
+package com.wallmannse.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallmannSePortalApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WallmannSePortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallmannse/portal/WallmannSePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallmannse.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallmannSePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
